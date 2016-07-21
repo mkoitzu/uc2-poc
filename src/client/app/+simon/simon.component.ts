68c647efb2d3bf6512e8dd59efc1db592ce718e3
unknown <VDS2088@uniqa.at>
2016-07-21 14:31:40 +0200

    Add simon page

---
+++ src/client/app/+simon/simon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+/**
+ * This class represents the lazy loaded AboutComponent.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'sd-simon',
+  templateUrl: 'simon.component.html',
+  styleUrls: ['simon.component.css']
+})
+export class SimonComponent {}
