02268563a51d6166e9c70a8599fd84f10db58ed6
Simon Kofler <VDS2088@uniqa.at>
2016-07-25 11:27:24 +0200

    Add Master/Detail, Multiple Components, Services

--- src/client/app/+simon/simon.component.ts
+++ src/client/app/+simon/simon.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 /**
  * This class represents the lazy loaded AboutComponent.
@@ -7,6 +10,23 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'sd-simon',
   templateUrl: 'simon.component.html',
-  styleUrls: ['simon.component.css']
+  styleUrls: ['simon.component.css'],
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
-export class SimonComponent {}
+export class SimonComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
+
+  getHeroes() {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+}
--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -4,12 +4,14 @@ import { AboutRoutes } from './+about/index';
 import { HomeRoutes }  from './+home/index';
 import { MirekRoutes } from './+mirek/index';
 import { GuiRoutes }   from './+gui/index';
+import {SimonRoutes} from "./+simon/simon.routes";
 
 const routes: RouterConfig = [
   ...HomeRoutes,
   ...AboutRoutes,
   ...MirekRoutes,
-  ...GuiRoutes
+  ...GuiRoutes,
+  ...SimonRoutes
 ];
 
 export const APP_ROUTER_PROVIDERS = [
