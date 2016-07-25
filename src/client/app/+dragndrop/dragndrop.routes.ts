359765d01a73f103bad050e3f0dabccd1da6e67b
simon <VDS2088@uniqa.at>
2016-07-25 14:35:48 +0200

    Add dragndrop page

---
+++ src/client/app/+dragndrop/dragndrop.routes.ts
@@ -0,0 +1,10 @@
+import { RouterConfig } from '@angular/router';
+
+import { DragNDropComponent } from './index';
+
+export const DragNDropRoutes: RouterConfig = [
+  {
+    path: 'dragndrop',
+    component: DragNDropComponent
+  },
+];
--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -4,14 +4,16 @@ import { AboutRoutes } from './+about/index';
 import { HomeRoutes }  from './+home/index';
 import { MirekRoutes } from './+mirek/index';
 import { GuiRoutes }   from './+gui/index';
-import { SimonRoutes } from "./+simon/simon.routes";
+import { SimonRoutes } from './+simon/simon.routes';
+import { DragNDropRoutes } from './+dragndrop/dragndrop.routes';
 
 const routes: RouterConfig = [
   ...HomeRoutes,
   ...AboutRoutes,
   ...MirekRoutes,
   ...GuiRoutes,
-  ...SimonRoutes
+  ...SimonRoutes,
+  ...DragNDropRoutes
 ];
 
 export const APP_ROUTER_PROVIDERS = [
